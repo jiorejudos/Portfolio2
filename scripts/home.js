178992123a7fea3e7cf1cd4269d30103248e4aba
Jeannine Orejudos <jiorejudos@Jeannines-MBP.PK5001Z>
2018-09-20 11:11:00 -0400

    mobile dropdown fix

--- scripts/home.js
+++ scripts/home.js
@@ -1,12 +1,16 @@
-document.onclick = function () {
-	// change div
-}
-div.onclick = function () {}
+(function() {
 
-document.onclick = function () {
-	var newDiv = document.createElement('div');
-	// populate div
-	newDiv.onclick = function () {};
-	document.body.appendChild(newDiv);
-}
-div.onclick = function () {}
+	var hamburger = {
+		navToggle: document.querySelector('.nav-toggle'),
+		nav: document.querySelector('nav'),
+
+		doToggle: function(e) {
+			e.preventDefault();
+			this.navToggle.classList.toggle('expanded');
+			this.nav.classList.toggle('expanded');
+		}
+	};
+
+	hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+
+}());
